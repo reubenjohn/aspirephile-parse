6bb81fdbceead55bdfbf1715ce620777f7080231
Reuben John <reubenvjohn@gmail.com>
2015-04-08 22:30:38 +0530

    Added lister to notify when a list of parse objects have been selected

---
+++ OnParseObjectsSelectedListener.java
@@ -0,0 +1,9 @@
+package com.aspirephile.shared.parse;
+
+import com.funtainment.event.field.Category;
+
+import java.util.ArrayList;
+
+public interface OnParseObjectsSelectedListener {
+    public void onParseObjectsSelectedListener(ArrayList<Category> selectedObjectIds);
+}
