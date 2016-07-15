09f3e9d8e50a6ff818c80551bc4a525dba998308
Reuben John <reubenvjohn@gmail.com>
2016-07-15 17:00:48 +0530

    CacheSyncManager uses local and online query set by adapter.

--- src/main/java/com/aspirephile/shared/parse/CacheSyncManager.java
+++ src/main/java/com/aspirephile/shared/parse/CacheSyncManager.java
@@ -17,45 +17,43 @@ import java.util.List;
 
 public class CacheSyncManager<T extends ParseObject> {
     private static final int defaultPageSize = 15;
-    private int pageSize = defaultPageSize;
-
-    private final String TAG = CacheSyncManager.class.getName();
+    private final Class<T> subClass;
+    ParseImageViewUtils pivUtils;
+    List<T> cachedList;
+    private String tag;// = CacheSyncManager.class.getName();
+    private final DatasetChangeListener defaultDatasetChangeListener = new DatasetChangeListener() {
+        @Override
+        public void onDatasetChanged() {
+            Log.w(tag, "Default dataset change listener has been called");
+        }
+    };
+    private Logger l;
     private final SyncListener<T> defaultSyncListener = new SyncListener<T>() {
         @Override
         public void onLoadingLocalList() {
-            Log.d(TAG, "Default local onLoading organization adapter callback");
+            l.w("Default local onLoading sync manager callback");
         }
 
         @Override
         public boolean onLoadedLocalList(List<T> list, ParseException e, List<T> oldList) {
-            Log.d(TAG, "Default local onLoaded organization adapter callback");
+            Log.w(tag, "Default local onLoaded sync manager callback");
             return true;
         }
 
         @Override
         public void onLoadingOnlineList() {
-            Log.d(TAG, "Default online onLoading organization adapter callback");
+            Log.w(tag, "Default online onLoading sync manager callback");
         }
 
         @Override
         public boolean onLoadedOnlineList(List<T> newList, ParseException e, List<T> oldList) {
-            Log.d(TAG, "Default online onLoaded organization adapter callback");
+            Log.w(tag, "Default online onLoaded sync manager callback");
             return true;
         }
     };
-    private final DatasetChangeListener defaultDatasetChangeListener = new DatasetChangeListener() {
-        @Override
-        public void onDatasetChanged() {
-            Log.w(TAG, "Default dataset change listener has been called");
-        }
-    };
-
+    private int pageSize = defaultPageSize;
     private SyncListener<T> syncListener;
     private DatasetChangeListener datasetChangeListener;
-    private final Class<T> subClass;
-    Logger l = new Logger(CacheSyncManager.class);
-    ParseImageViewUtils pivUtils = new ParseImageViewUtils(l);
-    List<T> cachedList;
     private NullPointerAsserter asserter = new NullPointerAsserter(l);
     private final FindCallback<T> localListLoadedCallback = new FindCallback<T>() {
         @Override
@@ -70,6 +68,7 @@ public class CacheSyncManager<T extends ParseObject> {
             }
         }
     };
+    private ParseQuery<T> localQuery;
     private final FindCallback<T> onlineListLoadedCallback = new FindCallback<T>() {
         @Override
         public void done(List<T> list, ParseException e) {
@@ -77,58 +76,71 @@ public class CacheSyncManager<T extends ParseObject> {
                 e.printStackTrace();
             } else if (syncListener.onLoadedOnlineList(list, e, cachedList)) {
                 l.d("List of size: " + list.size() + " loaded from server.");
-                repinListToLocalDatastore(list);
+                repinListToLocalDatastore(list, cachedList);
             } else {
-                l.w("Skipping updation of local datastore as per callback result");
+                l.w("Skipped updating of local datastore as per callback result");
             }
         }
     };
+    private ParseQuery<T> onlineQuery;
 
-    public CacheSyncManager(Class<T> subClass) {
+    public CacheSyncManager(Class<T> subClass, String tag) {
         this.subClass = subClass;
+        this.tag = CacheSyncManager.class.getSimpleName() + "_" + tag;
+        l = new Logger(tag);
+        pivUtils = new ParseImageViewUtils(l);
 
         syncListener = defaultSyncListener;
         datasetChangeListener = defaultDatasetChangeListener;
 
         // To prevent null pointer exception if the list is accessed before the callback from local datastore query
         cachedList = new ArrayList<>();
-        refreshListFromLocalDatastore();
-        updateLocalDatastore();
+    }
+
+    public static <T extends ParseObject> boolean isListIDsEquivalent(List<T> cachedList, List<T> list) {
+        if (cachedList == list)
+            return true;
+        if (cachedList.size() == list.size()) {
+            for (int i = 0; i < cachedList.size(); i++) {
+                if (!cachedList.get(i).getObjectId().equals(list.get(i).getObjectId())) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
     public void refreshListFromLocalDatastore() {
         l.d("Refreshing list from local datastore");
         syncListener.onLoadingLocalList();
-        getLocalOrganizationListInBackground(localListLoadedCallback);
+        getLocalListInBackground(localListLoadedCallback);
     }
 
     public void updateLocalDatastore() {
-        l.d("Updating local organization datastore");
+        l.d("Updating local datastore");
         syncListener.onLoadingOnlineList();
         fetchListFromServer(onlineListLoadedCallback);
     }
 
     private void fetchListFromServer(FindCallback<T> callback) {
-        ParseQuery<T> query = ParseQuery.getQuery(subClass);
-        // TODO Fetch only organizations that the user belongs to
-        // TODO Select only required keys
-        //organizationQuery.include(Organization.fields....);
-        query.findInBackground(callback);
+        onlineQuery.findInBackground(callback);
     }
 
-    private void repinListToLocalDatastore(final List<T> list) {
-        l.d("Pinning list of size: " + list.size() + " in background");
-        T.unpinAllInBackground(new DeleteCallback() {
+    private void repinListToLocalDatastore(final List<T> list, final List<T> oldList) {
+        l.d("Unpinning local list from local datastore");
+        T.unpinAllInBackground(tag, new DeleteCallback() {
             @Override
             public void done(ParseException e) {
                 if (list.size() > 0) {
-                    for (T organization : list) {
+                    for (T t : list) {
                         ParseACL publicACL = new ParseACL();
                         publicACL.setPublicReadAccess(true);
                         publicACL.setPublicWriteAccess(true);
-                        organization.setACL(publicACL);
+                        t.setACL(publicACL);
                     }
-                    T.pinAllInBackground(list, new SaveCallback() {
+                    l.d("Pinning list of size: " + list.size() + " in background");
+                    T.pinAllInBackground(tag, list, new SaveCallback() {
                         @Override
                         public void done(ParseException e) {
                             if (asserter.assertPointerQuietly(e)) {
@@ -146,12 +158,8 @@ public class CacheSyncManager<T extends ParseObject> {
         });
     }
 
-    public void getLocalOrganizationListInBackground(FindCallback<T> callback) {
-        ParseQuery<T> localQuery = ParseQuery.getQuery(subClass);
-        localQuery
-                .setLimit(pageSize)
-                .fromLocalDatastore()
-                .findInBackground(callback);
+    public void getLocalListInBackground(FindCallback<T> callback) {
+        localQuery.findInBackground(callback);
     }
 
     public void setSyncListener(SyncListener<T> syncListener) {
@@ -169,28 +177,26 @@ public class CacheSyncManager<T extends ParseObject> {
 
     public void setCachedList(List<T> cachedList) {
         if (asserter.assertPointer(cachedList)) {
-            l.d("Setting organization list of size: " + cachedList.size());
+            l.d("Setting list of size: " + cachedList.size());
             this.cachedList = cachedList;
             datasetChangeListener.onDatasetChanged();
         }
     }
 
-    public static <T extends ParseObject> boolean isListIDsEquivalent(List<T> cachedList, List<T> list) {
-        if (cachedList == list)
-            return true;
-        if (cachedList.size() == list.size()) {
-            for (int i = 0; i < cachedList.size(); i++) {
-                if (!cachedList.get(i).getObjectId().equals(list.get(i).getObjectId())) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-
     public void setOnDatasetChangedListener(DatasetChangeListener onDatasetChangedListener) {
         this.datasetChangeListener = onDatasetChangedListener;
+    }
+
+    public void setLocalQuery(ParseQuery<T> localQuery) {
+        this.localQuery = localQuery;
+    }
+
+    public void setOnlineQuery(ParseQuery<T> onlineQuery) {
+        this.onlineQuery = onlineQuery;
+    }
+
+    public String getPinTag() {
+        return tag;
     }
 
     public interface SyncListener<T extends ParseObject> {
---
+++ src/main/java/com/aspirephile/shared/parse/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package com.aspirephile.shared.parse;
+
+/**
+ * Created by Reuben John on 7/8/2016.
+ */
+public interface OnItemClickListener<T> {
+    void onItemClick(T item);
+}
